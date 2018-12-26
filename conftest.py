eb547f0ba9c385b2858a9c3dbffc71717759f6e1
b0ses <b0ses@users.noreply.github.com>
2018-12-26 18:02:30 -0500

    Updating tests for groups

--- conftest.py
+++ conftest.py
@@ -6,36 +6,26 @@ from flask_migrate import upgrade
 from custom_stream_api.shared import create_app
 
 
-@pytest.fixture
-def test_client():
-    app, socketio, db, migrate = create_app(init_db=False)
+@pytest.fixture(scope='session')
+def app(request):
     db_fd, temp_db_path = tempfile.mkstemp()
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(temp_db_path)
-    app.config['TESTING'] = True
-    client = app.test_client()
-
-    with app.app_context():
-        db.init_app(app)
-        upgrade(migrate.directory)
-
-    yield client
-
-    os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
-
-
-@pytest.fixture
-def test_app():
+    settings_override = {
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///{}'.format(temp_db_path),
+        'TESTING': True
+    }
     app, socketio, db, migrate = create_app(init_db=False)
-    db_fd, temp_db_path = tempfile.mkstemp()
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(temp_db_path)
-    app.config['TESTING'] = True
+    for setting in settings_override:
+        app.config[setting] = settings_override[setting]
 
-    with app.app_context():
-        db.init_app(app)
-        upgrade(migrate.directory)
+    ctx = app.app_context()
+    ctx.push()
+    db.init_app(app)
+    upgrade(migrate.directory)
 
-    yield app
+    def teardown():
+        db.drop_all()
+        ctx.pop()
+        os.close(db_fd)
+        os.remove(temp_db_path)
 
-    os.close(db_fd)
-    os.unlink(temp_db_path)
+    request.addfinalizer(teardown)
