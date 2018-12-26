a9aa817501104acc19398caf684a00b62bb20560
b0ses <b0ses@users.noreply.github.com>
2018-12-26 15:37:13 -0500

    Debug setting

--- custom_stream_api/settings.py
+++ custom_stream_api/settings.py
@@ -1,3 +1,5 @@
+DEBUG = True
+
 # Where you want the server to be hosted, change if you want this to work remotely
 # Note: the Stream Overlay and Dashboard need to match this in order for all of this to work!
 HOST = '127.0.0.1'
