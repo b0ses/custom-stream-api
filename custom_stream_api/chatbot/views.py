39f4cb3ee22cb1a3bd8c477a698054c20d06484a
b0ses <b0ses@users.noreply.github.com>
2019-03-09 19:07:48 -0500

    Updated chatbot permissions and aliases

--- custom_stream_api/chatbot/views.py
+++ custom_stream_api/chatbot/views.py
@@ -5,6 +5,7 @@ from custom_stream_api.shared import InvalidUsage
 
 chatbot_endpoints = Blueprint('chatbot', __name__)
 
+from custom_stream_api.shared import g
 
 @chatbot_endpoints.route('/start', methods=['POST'])
 def start_post():
@@ -36,3 +37,54 @@ def stop_post():
             return jsonify({'message': 'stopped chatbot'})
         except Exception as e:
             raise InvalidUsage(str(e))
+
+
+@chatbot_endpoints.route('/aliases', methods=['GET', 'POST'])
+def aliases():
+    if 'chatbot' not in g:
+        raise InvalidUsage('No chatbot running. Please start the chatbot beforehand.')
+    if request.method == 'GET':
+        try:
+            all_aliases = [alias.as_dict() for alias in g['chatbot'].list_aliases()]
+        except Exception as e:
+            raise InvalidUsage(str(e))
+        return jsonify(all_aliases)
+    else:
+        data = request.get_json()
+        try:
+            g['chatbot'].import_aliases(data)
+        except Exception as e:
+            raise InvalidUsage(str(e))
+        return jsonify({'message': 'Chat reactions imported'})
+
+
+@chatbot_endpoints.route('/add_alias', methods=['POST'])
+def add_alias_post():
+    data = request.get_json()
+    alias_data = {
+        'alias': data.get('alias', ''),
+        'command': data.get('command', ''),
+        'badge': data.get('badge', 'broadcaster')
+    }
+    chatbot_id = data.get('chatbot_id', '')
+    try:
+        chatbot = twitchbot.verify_chatbot_id(chatbot_id)
+        chatbot.add_alias(**alias_data)
+        return jsonify({'message': 'Alias added'})
+    except Exception as e:
+        raise InvalidUsage(str(e))
+
+
+@chatbot_endpoints.route('/remove_alias', methods=['POST'])
+def remove_alias_post():
+    data = request.get_json()
+    alias_data = {
+        'alias': data.get('alias', '')
+    }
+    chatbot_id = data.get('chatbot_id', '')
+    try:
+        chatbot = twitchbot.verify_chatbot_id(chatbot_id)
+        chatbot.remove_alias(**alias_data)
+        return jsonify({'message': 'Alias removed'})
+    except Exception as e:
+        raise InvalidUsage(str(e))
