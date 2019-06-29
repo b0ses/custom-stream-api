4a1cf883906dd3133175364f019354b0056095ac
b0ses <b0ses@users.noreply.github.com>
2019-06-29 18:43:43 -0400

    Initial attempt at setup_webhook endpoint

--- custom_stream_api/notifier/twitch_notifier.py
+++ custom_stream_api/notifier/twitch_notifier.py
@@ -1,11 +1,15 @@
 import requests
 import json
-from custom_stream_api.settings import APP_CLIENT_CRED_TOKEN
+import threading
+import time
+from custom_stream_api import settings
 
 TOKEN_URL = 'https://id.twitch.tv/oauth2/'
 HELIX_URL = 'https://api.twitch.tv/helix'
 WEBHOOKS_URL = '{}/webhooks'.format(HELIX_URL)
-
+WEBHOOK_SECRET = settings.WEBHOOK_SECRET
+WEBHOOK_LEASE = 60 * 60 * 24 # 24 hours, daily
+WEBHOOK_RENEW = WEBHOOK_LEASE - (60 * 60) # 23 hours so that it renews before expiring
 
 def get_user_data(token, username):
     params = {
@@ -65,16 +69,44 @@ def setup_stream_changed_webhook(token, user_id):
         return json.loads(response.content.decode())
 
 
-def subscribe_webhook(callback_url, mode, topic, token, secret=None):
+def _subscribe_webhook(data, token):
+    return requests.post('{}/hub'.format(WEBHOOKS_URL), headers=get_headers(token), data=data)
+
+
+def _renew_subscription(subscription_data):
+    lease_timer = 0
+    # TODO: trigger/flag to kill renew_subscription threads
+    while True:
+        if lease_timer == WEBHOOK_RENEW:
+            _subscribe_webhook(subscription_data)
+            lease_timer = 0
+        time.sleep(1)
+        lease_timer += 1
+
+
+def setup_webhook(callback_url, mode, topic, token):
+    if not (callback_url and mode and topic and token):
+        raise Exception('Not enough arguments provided (callback_url, mode, topic, or token can\'t be None')
+    if mode not in ['subscribe', 'unsubscribe']:
+        raise Exception('Mode must be subscribe or unsubscribe')
+    if topic not in ['User Follows', 'Stream Changed']:
+        raise Exception('Topic must be \'User Follows\' or \'Stream Changed\'')
     data = {
         'hub.callback': callback_url,
         'hub.mode': mode,
         'hub.topic': topic,
+        'hub.lease_seconds': WEBHOOK_LEASE
     }
+    secret = WEBHOOK_SECRET
     if secret:
         data['hub.secret'] = secret
+    else:
+        raise('WEBHOOK_SECRET setting not setup. Please just fill it in to continue.')
 
-    response = requests.post('{}/hub'.format(WEBHOOKS_URL), headers=get_headers(token), data=data)
-    print(response)
+    response = _subscribe_webhook(data, token)
     if response.status_code == 200:
+        renew_thread = threading.Thread(target=_renew_subscription, args=(data, token))
+        renew_thread.start()
         return json.loads(response.content.decode())
+    else:
+        raise Exception('Failed to setup hook')
