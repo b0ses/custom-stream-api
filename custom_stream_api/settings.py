ed54488f1439a9d266918ac559632f4bbe38b1f6
b0ses <b0ses@users.noreply.github.com>
2019-08-27 22:22:10 -0400

    Updating settings to include client details

--- custom_stream_api/settings.py
+++ custom_stream_api/settings.py
@@ -4,10 +4,18 @@ DEBUG = True
 # Note: the Stream Overlay and Dashboard need to match this in order for all of this to work!
 HOST = '127.0.0.1'
 PORT = 5000
+DASHBOARD_PORT = 8080
+OVERLAY_PORT = None
 
 # Just fill these in with whatever. Adds security to your server.
 SECRET = ''
 WEBHOOK_SECRET = ''
+
+# Twitch Login
+LOGIN = False
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+REDIRECT_URI = ''
 
 # Chatbot Settings
 TIMEOUT = 15  # seconds between spamming sounds
