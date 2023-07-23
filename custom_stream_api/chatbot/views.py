8056bb1dff1c68e6d5066a196506a87620a60099
b0ses <b0ses@users.noreply.github.com>
2023-07-22 22:50:49 -0400

    Cleaning up chatbot

--- custom_stream_api/chatbot/views.py
+++ custom_stream_api/chatbot/views.py
@@ -7,34 +7,34 @@ from custom_stream_api.shared import InvalidUsage
 from custom_stream_api.chatbot import aliases, timers
 from custom_stream_api.auth import twitch_auth
 
-chatbot_endpoints = Blueprint('chatbot', __name__)
+chatbot_endpoints = Blueprint("chatbot", __name__)
 
 logger = logging.getLogger()
 
 
-@chatbot_endpoints.route('/restart', methods=['POST'])
+@chatbot_endpoints.route("/restart", methods=["POST"])
 @twitch_auth.twitch_login_required
 def start_post():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.get_json()
         twitchbot_data = {
-            'bot_name': data.get('bot_name', ''),
-            'client_id': data.get('client_id', ''),
-            'chat_token': data.get('chat_token', ''),
-            'channel': data.get('channel', ''),
-            'timeout': data.get('timeout', 15)
+            "bot_name": data.get("bot_name", ""),
+            "client_id": data.get("client_id", ""),
+            "chat_token": data.get("chat_token", ""),
+            "channel": data.get("channel", ""),
+            "timeout": data.get("timeout", 15),
         }
         try:
             bot_id = twitchbot.setup_chatbot(**twitchbot_data)
-            return jsonify({'message': 'started chatbot: {}'.format(bot_id)})
+            return jsonify({"message": "started chatbot: {}".format(bot_id)})
         except Exception as e:
             raise InvalidUsage(str(e))
 
 
-@chatbot_endpoints.route('/aliases', methods=['GET', 'POST'])
+@chatbot_endpoints.route("/aliases", methods=["GET", "POST"])
 @twitch_auth.twitch_login_required
 def list_aliases_get():
-    if request.method == 'GET':
+    if request.method == "GET":
         try:
             all_aliases = aliases.list_aliases()
         except Exception as e:
@@ -47,43 +47,41 @@ def list_aliases_get():
             aliases.import_aliases(data)
         except Exception as e:
             raise InvalidUsage(str(e))
-        return jsonify({'message': 'Aliases imported'})
+        return jsonify({"message": "Aliases imported"})
 
 
-@chatbot_endpoints.route('/add_alias', methods=['POST'])
+@chatbot_endpoints.route("/add_alias", methods=["POST"])
 @twitch_auth.twitch_login_required
 def add_alias_post():
     data = request.get_json()
     alias_data = {
-        'alias': data.get('alias', ''),
-        'command': data.get('command', ''),
-        'badge': data.get('badge', 'broadcaster')
+        "alias": data.get("alias", ""),
+        "command": data.get("command", ""),
+        "badge": data.get("badge", "broadcaster"),
     }
     try:
         aliases.add_alias(**alias_data)
-        return jsonify({'message': 'Alias added'})
+        return jsonify({"message": "Alias added"})
     except Exception as e:
         raise InvalidUsage(str(e))
 
 
-@chatbot_endpoints.route('/remove_alias', methods=['POST'])
+@chatbot_endpoints.route("/remove_alias", methods=["POST"])
 @twitch_auth.twitch_login_required
 def remove_alias_post():
     data = request.get_json()
-    alias_data = {
-        'alias': data.get('alias', '')
-    }
+    alias_data = {"alias": data.get("alias", "")}
     try:
         aliases.remove_alias(**alias_data)
-        return jsonify({'message': 'Alias removed'})
+        return jsonify({"message": "Alias removed"})
     except Exception as e:
         raise InvalidUsage(str(e))
 
 
-@chatbot_endpoints.route('/timers', methods=['GET', 'POST'])
+@chatbot_endpoints.route("/timers", methods=["GET", "POST"])
 @twitch_auth.twitch_login_required
 def list_timers_get():
-    if request.method == 'GET':
+    if request.method == "GET":
         try:
             all_timers = timers.list_timers()
         except Exception as e:
@@ -96,33 +94,28 @@ def list_timers_get():
             timers.import_timers(data)
         except Exception as e:
             raise InvalidUsage(str(e))
-        return jsonify({'message': 'Timers imported'})
+        return jsonify({"message": "Timers imported"})
 
 
-@chatbot_endpoints.route('/add_timer', methods=['POST'])
+@chatbot_endpoints.route("/add_timer", methods=["POST"])
 @twitch_auth.twitch_login_required
 def add_timer_post():
     data = request.get_json()
-    timer_data = {
-        'command': data.get('command', ''),
-        'interval': data.get('interval', 30)
-    }
+    timer_data = {"command": data.get("command", ""), "interval": data.get("interval", 30)}
     try:
         timers.add_timer(**timer_data)
-        return jsonify({'message': 'Timer added'})
+        return jsonify({"message": "Timer added"})
     except Exception as e:
         raise InvalidUsage(str(e))
 
 
-@chatbot_endpoints.route('/remove_timer', methods=['POST'])
+@chatbot_endpoints.route("/remove_timer", methods=["POST"])
 @twitch_auth.twitch_login_required
 def remove_timer_post():
     data = request.get_json()
-    timer_data = {
-        'command': data.get('command', '')
-    }
+    timer_data = {"command": data.get("command", "")}
     try:
         timers.remove_timer(**timer_data)
-        return jsonify({'message': 'Timer removed'})
+        return jsonify({"message": "Timer removed"})
     except Exception as e:
         raise InvalidUsage(str(e))
