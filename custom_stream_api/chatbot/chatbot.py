98d21de95c2f8015eeecbcba053633dd2fcc2417
b0ses <b0ses@users.noreply.github.com>
2025-11-22 17:51:33 -0500

    black

--- custom_stream_api/chatbot/chatbot.py
+++ custom_stream_api/chatbot/chatbot.py
@@ -413,18 +413,18 @@ class ChatBot:
     def chat_count_output(self, count_name, count):
         if count is not None:
             emoji_mapper = {
-                '0': ':zero:',
-                '1': ':one:',
-                '2': ':two:',
-                '3': ':three:',
-                '4': ':four:',
-                '5': ':five:',
-                '6': ':six:',
-                '7': ':seven:',
-                '8': ':eight:',
-                '9': ':nine:',
+                "0": ":zero:",
+                "1": ":one:",
+                "2": ":two:",
+                "3": ":three:",
+                "4": ":four:",
+                "5": ":five:",
+                "6": ":six:",
+                "7": ":seven:",
+                "8": ":eight:",
+                "9": ":nine:",
             }
-            emoji_count = ' '.join([emoji_mapper[char] for char in str(count)])
+            emoji_count = " ".join([emoji_mapper[char] for char in str(count)])
             self.chat("{}: {}".format(count_name, emoji_count))
 
     def copy_count(self, text):
