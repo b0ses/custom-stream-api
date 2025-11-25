0603c1e797edc72092336293d8a0b79a9c6322dd
b0ses <b0ses@users.noreply.github.com>
2025-11-24 19:19:20 -0500

    Setting timezones to timers

--- custom_stream_api/chatbot/timers.py
+++ custom_stream_api/chatbot/timers.py
@@ -2,7 +2,9 @@ import logging
 import threading
 from cron_converter import Cron
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
+from custom_stream_api.settings import TIMER_TZ
 from custom_stream_api.chatbot.models import Timer
 from custom_stream_api.shared import run_async_in_thread, db
 
@@ -17,20 +19,24 @@ SUPPORTED_BOTS = {
 
 INTERRUPTER = threading.Event()
 
+if TIMER_TZ:
+    TIMER_TZ = ZoneInfo(TIMER_TZ)
+
 
 def list_timers():
     return [timer.as_dict() for timer in db.session.query(Timer).order_by(Timer.command.asc()).all()]
 
 
+# Note: Next time values in the database are *not* UTC like the rest of the database, it's relative to TIMER_TZ
 def calculate_next_time(cron):
-    now = datetime.now().astimezone()
+    now = datetime.now(TIMER_TZ)
     cron_instance = Cron(cron)
     schedule = cron_instance.schedule(now)
     return schedule.next()
 
 
 def check_timers(app, db, execute=True):
-    now = datetime.now().astimezone()
+    now = datetime.now(TIMER_TZ)
     timers = db.session.query(Timer).filter(Timer.next_time <= now, Timer.active.is_(True))
     for timer in timers:
         if execute:
@@ -95,7 +101,7 @@ async def scheduler_in_background(app, db, scheduler_event):
             if db.session.query(Timer).count():
                 # If there are timers, wait until the earliest timer or an interruption
                 next_time = db.session.query(Timer.next_time).order_by(Timer.next_time.asc()).first()[0]
-                now = datetime.now().astimezone()
+                now = datetime.now(TIMER_TZ)
                 time_to_wait = (next_time - now).total_seconds()
                 logger.info(f"Waiting for next signal: {time_to_wait}")
             else:
